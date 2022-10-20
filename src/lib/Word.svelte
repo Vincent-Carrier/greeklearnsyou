4b1d63a675f5874f967e71ad36fa7fa4de39ccf8
Vincent Carrier <vincentcarrier15@gmail.com>
2022-10-20 10:59:27 -0400

    Add href to wiktionary

--- src/lib/Word.svelte
+++ src/lib/Word.svelte
@@ -5,16 +5,18 @@
 	export let word: Word;
 </script>
 
-<FancyBox>
-	<header class="text-3xl text-stone-600 font-bold">
-		<div class="">{word.root}</div>
-		<div class="font-normal mt-1 text-stone-500 text-xl">{word.meaning}</div>
-	</header>
-
-	<p class="mt-3 text-sm text-stone-500">
-		<span class="font-bold">Derivatives: </span>{word.derivates.join(', ')}
-	</p>
-</FancyBox>
+<a href={`https://en.wiktionary.org/wiki/${word.root}`} target="_blank" rel="noreferrer">
+	<FancyBox>
+		<header class="text-3xl text-stone-600 font-bold">
+			<div class="">{word.root}</div>
+			<div class="font-normal mt-1 text-stone-500 text-xl">{word.meaning}</div>
+		</header>
+	
+		<p class="mt-3 text-sm text-stone-500">
+			<span class="font-bold">Derivatives: </span>{word.derivates.join(', ')}
+		</p>
+	</FancyBox>
+</a>
 
 <style>
 </style>
