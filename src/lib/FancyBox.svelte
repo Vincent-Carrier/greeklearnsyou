2efafc5c17a2c95b19eb553ddd2eef4990b6baf7
Vincent Carrier <vincentcarrier15@gmail.com>
2022-10-24 13:44:52 -0400

    Make it fancier

--- src/lib/FancyBox.svelte
+++ src/lib/FancyBox.svelte
@@ -3,14 +3,8 @@
 </script>
 
 <article
-	class="relative shadow-md rounded-md border-2 border-yellow-300 outline outline-2 outline-offset-[-8px] outline-yellow-300 p-8 bg-stone-50"
+	class="relative shadow-md rounded-md border-[3px] border-amber-400 outline-dashed outline-4 outline-offset-[-16px] transition-all hover:outline-offset-4 outline-slate-300 p-8 bg-stone-50"
 >
-	{#each ['top-1 left-1', 'top-1 right-1', 'bottom-1 left-1', 'bottom-1 right-1'] as pos}
-		<svg class={`absolute h-4 w-4 ${pos}`} viewBox="0 0 100 100">
-			<rect width="96" height="96" stroke-width="10" class="stroke-yellow-300 fill-transparent" />
-			<circle cx="45" cy="45" r="10" class="fill-yellow-300" />
-		</svg>
-	{/each}
 	<slot />
 </article>
 
